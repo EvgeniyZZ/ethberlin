a378e4778c20ffd060e12b014e2fba840d223fe8
olehnikolaiev <oleg@skalelabs.com>
2019-08-25 11:57:22 +0300

    esklhk

--- src/validatorContract.js
+++ src/validatorContract.js
@@ -1,15 +1,15 @@
-const configJson = require("./config.json")
-const abi = configJson["contractValidator"]["abi"];
-const contractAddress = configJson["contractValidator"]["address"];
+const configJson = require('./config.json');
+const abi = configJson['Validator']['abi'];
+const contractAddress = configJson['Validator']['address'];
 
 class ValidatorContract {
-    constructor(web3) {
-        this.web3 = web3;
-        this.contract = new web3.eth.Contract(abi, contractAddress);
-    }
+	constructor (web3) {
+		this.web3 = web3;
+		this.contract  = new web3.eth.Contract(abi, contractAddress);
+	}
 
-    async validate(address owner, string filename) {
-        let result = this.contract.methods.validate(owner, string filename);
-        return result;
-    }
+	async validate(address, fileName) {
+		let result = this.contract.methods.validate(address, fileName);
+		return result;
+	}
 }
